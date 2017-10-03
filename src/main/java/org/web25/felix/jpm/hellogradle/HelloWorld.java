b9f5ef28b8e8383d7f9b9e6f502a68cc4b463e3f
felix <felix.resch@web25.org>
2017-10-03 13:39:19 +0200

    added http stuff

--- src/main/java/org/web25/felix/jpm/hellogradle/HelloWorld.java
+++ src/main/java/org/web25/felix/jpm/hellogradle/HelloWorld.java
@@ -1,8 +1,17 @@
 package org.web25.felix.jpm.hellogradle;
 
+import io.femo.http.Http;
+import io.femo.http.HttpServer;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        HttpServer server = Http
+                .server(8080)
+                .get("/", (request, response) -> {
+                    response.entity("Hello World");
+                    return true;
+                });
+        server.start();
     }
 }
