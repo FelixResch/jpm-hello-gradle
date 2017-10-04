4cba21b743244107ad91f64318e790aa537402bd
felix <felix.resch@web25.org>
2017-10-04 14:39:24 +0200

    added http logging

--- src/main/java/org/web25/felix/jpm/hellogradle/HelloWorld.java
+++ src/main/java/org/web25/felix/jpm/hellogradle/HelloWorld.java
@@ -2,12 +2,14 @@ package org.web25.felix.jpm.hellogradle;
 
 import io.femo.http.Http;
 import io.femo.http.HttpServer;
+import io.femo.http.handlers.Handlers;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
         HttpServer server = Http
                 .server(8080)
+                .use(Handlers.log())
                 .get("/", (request, response) -> {
                     response.entity("Hello World");
                     return true;
